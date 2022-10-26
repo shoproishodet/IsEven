aeee902b9eed0357a7bd2b3a8056aab18b998602
lebbriz <grudinator@icloud.com>
2022-10-26 18:21:35 +0300

    fix_repo_error

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-#include <bitset>
 
 bool is_even(int num) {
-    std::bitset<1> first_bit (num);
-    return first_bit.test(0) == 0;
+    return !(num & 1);
 }
 
 int main() {
-    int num = 150;
-    std::cout << (is_even(num) ? "Is even" : "Isn't even");
+    int num = 124032589;
+    std::cout << (is_even(num) ? "Is even" : "Is odd");
 }
